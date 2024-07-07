0fb4836aaa364909445b7ed17659f5ad21594b38
AdhvikaS <adhvikasundaramurali@gmail.com>
2024-07-07 13:48:49 +0530

    BMI

--- main (1).py
+++ main (1).py
@@ -1,7 +1,7 @@
 #Adhvika
 a=float(input("Enter Weight "))
 b=float(input("Enter Height "))
-BMI=a/b**2
+BMI=a/(b/100)**2
 if BMI<18.:
     print("Underweight")
 if 18.5 < BMI < 24.9:
@@ -9,5 +9,5 @@ if 18.5 < BMI < 24.9:
 if 25 < BMI < 29.9:
     print("Over Weight")
 if BMI > 30:
-    print("Obsity")
-
+    print("Obsity") 
+ 
